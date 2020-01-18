c23b8fff6f8a42a00cf17ecf4b7d27245fa87b16
Mauricio <mauriciofigueiredo@knights.ucf.edu>
2020-01-18 19:01:06 +0100

    First pa01 commit

---
+++ src/costa_pa01/GoodbyeWorld.java
@@ -0,0 +1,10 @@
+package costa_pa01;
+
+public class GoodbyeWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%s", "Goodbye, World!");
+	}
+
+}
